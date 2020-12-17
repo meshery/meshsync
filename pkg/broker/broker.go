1a1d5dff736470aad65f8b5ed6ada66e289fdd83
Dev Kalra <kalradev@outlook.com>
2020-12-17 20:11:39 +0530

    package moved from operator to meshsync
    Signed-off-by: Dev Kalra <kalradev@outlook.com>

---
+++ pkg/broker/broker.go
@@ -0,0 +1,41 @@
+package broker
+
+import (
+	"github.com/layer5io/meshsync/pkg/broker/nats"
+)
+
+type Message struct {
+	Type   string
+	Object interface{}
+}
+
+type PublishInterface interface {
+	Publish(string, interface{}) error
+	PublishWithCallback(string, string, interface{}) error
+}
+
+type SubscribeInterface interface {
+	Subscribe(string, string) error
+	SubscribeWithHandler(string, string) error
+}
+
+type Handler interface {
+	PublishInterface
+	SubscribeInterface
+}
+
+const (
+	NATSKey = "nats"
+)
+
+func New(kind, url string) (Handler, error) {
+	var broker Handler
+	// switch kind {
+	// case NATSKey:
+	// 	return nats.New(url)
+	// }
+	if kind == NATSKey {
+		return nats.New(url)
+	}
+	return broker, nil
+}
