8e0501dcec1f451489e991a1ee4852c709005e4e
Dev Kalra <kalradev@outlook.com>
2020-12-22 18:22:51 +0530

    model package moved to pkg folder
    Signed-off-by: Dev Kalra <kalradev@outlook.com>

--- internal/meshes/istio/informers/workload_group.go
+++ internal/meshes/istio/informers/workload_group.go
@@ -3,7 +3,7 @@ package informers
 import (
 	"log"
 
-	"github.com/layer5io/meshsync/internal/model"
+	"github.com/layer5io/meshsync/pkg/model"
 	broker "github.com/layer5io/meshsync/pkg/broker"
 	v1alpha3 "istio.io/client-go/pkg/apis/networking/v1alpha3"
 	"k8s.io/client-go/tools/cache"
--- internal/meshes/istio/pipeline/gateway.go
+++ internal/meshes/istio/pipeline/gateway.go
@@ -3,7 +3,7 @@ package pipeline
 import (
 	"log"
 
-	"github.com/layer5io/meshsync/internal/model"
+	"github.com/layer5io/meshsync/pkg/model"
 	broker "github.com/layer5io/meshsync/pkg/broker"
 	discovery "github.com/layer5io/meshsync/pkg/discovery"
 	"github.com/myntra/pipeline"
