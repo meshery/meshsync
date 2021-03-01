d4afc8126e8a20eb2c387bca0d83cf9a92534198
kumarabd <abishekkumar92@gmail.com>
2021-03-01 23:22:58 +0530

    added logging
    Signed-off-by: kumarabd <abishekkumar92@gmail.com>

--- internal/pipeline/local_resource.go
+++ internal/pipeline/local_resource.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	broker "github.com/layer5io/meshkit/broker"
+	"github.com/layer5io/meshkit/logger"
 	internalconfig "github.com/layer5io/meshsync/internal/config"
 	"github.com/layer5io/meshsync/pkg/model"
 	"github.com/myntra/pipeline"
@@ -15,13 +16,15 @@ import (
 
 type LocalResource struct {
 	pipeline.StepContext
+	log           logger.Handler
 	dynamicClient dynamic.Interface
 	brokerClient  broker.Handler
 	config        internalconfig.PipelineConfig
 }
 
-func NewLocalResource(dclient dynamic.Interface, bclient broker.Handler, config internalconfig.PipelineConfig) *LocalResource {
+func NewLocalResource(log logger.Handler, dclient dynamic.Interface, bclient broker.Handler, config internalconfig.PipelineConfig) *LocalResource {
 	return &LocalResource{
+		log:           log,
 		dynamicClient: dclient,
 		brokerClient:  bclient,
 		config:        config,
@@ -36,10 +39,12 @@ func (c *LocalResource) Exec(request *pipeline.Request) *pipeline.Result {
 		Resource: c.config.Resource,
 	}).Namespace(c.config.Namespace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
+		c.log.Error(err)
 		return &pipeline.Result{
 			Error: ErrList(c.config.Resource, err),
 		}
 	}
+	c.log.Info("discovering: ", c.config.Resource)
 
 	for _, item := range result.Items {
 		err = c.brokerClient.Publish(c.config.PublishSubject, &broker.Message{
@@ -48,6 +53,7 @@ func (c *LocalResource) Exec(request *pipeline.Request) *pipeline.Result {
 			Object:     model.ParseList(item),
 		})
 		if err != nil {
+			c.log.Error(err)
 			return &pipeline.Result{
 				Error: ErrPublish(c.config.Resource, err),
 			}
