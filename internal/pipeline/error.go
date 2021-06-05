16ad6c91321081eed41bfd269d71a16193d8f892
kumarabd <abishekkumar92@gmail.com>
2021-06-05 20:23:19 +0530

    added error utils
    Signed-off-by: kumarabd <abishekkumar92@gmail.com>

--- internal/pipeline/error.go
+++ internal/pipeline/error.go
@@ -5,19 +5,19 @@ import (
 )
 
 const (
-	ErrListCode          = "test_code"
-	ErrPublishCode       = "test_code"
-	ErrDynamicClientCode = "test_code"
+	ErrListCode          = "1001"
+	ErrPublishCode       = "1002"
+	ErrDynamicClientCode = "1003"
 )
 
 func ErrDynamicClient(name string, err error) error {
-	return errors.NewDefault(ErrDynamicClientCode, "Error creating dynamic client for: "+name, err.Error())
+	return errors.New(ErrDynamicClientCode, errors.Alert, []string{"Error creating dynamic client for: " + name, err.Error()}, []string{}, []string{}, []string{})
 }
 
 func ErrList(name string, err error) error {
-	return errors.NewDefault(ErrListCode, "Error while listing: "+name, err.Error())
+	return errors.New(ErrListCode, errors.Alert, []string{"Error while listing: " + name, err.Error()}, []string{}, []string{}, []string{})
 }
 
 func ErrPublish(name string, err error) error {
-	return errors.NewDefault(ErrPublishCode, "Error while publishing for: "+name, err.Error())
+	return errors.New(ErrPublishCode, errors.Alert, []string{"Error while publishing for: " + name, err.Error()}, []string{}, []string{}, []string{})
 }
