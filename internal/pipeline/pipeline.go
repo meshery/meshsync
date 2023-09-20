96f0bbf52f42eafb86b734eb18d31527cefb26d6
Mohd Uzair <muzair.shaikh810@gmail.com>
2023-09-21 00:34:39 +0530

    Revert "[Enhancement] Use Streaming Lists when available"

--- internal/pipeline/pipeline.go
+++ internal/pipeline/pipeline.go
@@ -5,7 +5,6 @@ import (
 	"github.com/layer5io/meshkit/logger"
 	internalconfig "github.com/layer5io/meshsync/internal/config"
 	"github.com/myntra/pipeline"
-	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/dynamic/dynamicinformer"
 )
 
@@ -30,48 +29,30 @@ var (
 	}
 )
 
-func New(log logger.Handler, informer dynamicinformer.DynamicSharedInformerFactory, broker broker.Handler, plConfigs map[string]internalconfig.PipelineConfigs, stopChan chan struct{}, dynamicKube dynamic.Interface) *pipeline.Pipeline {
-	// TODO: best way to check whether WatchList feature is enabled
-	watchList := true
-
+func New(log logger.Handler, informer dynamicinformer.DynamicSharedInformerFactory, broker broker.Handler, plConfigs map[string]internalconfig.PipelineConfigs, stopChan chan struct{}) *pipeline.Pipeline {
 	// Global discovery
 	gdstage := GlobalDiscoveryStage
 	configs := plConfigs[gdstage.Name]
-	if watchList {
-		for _, config := range configs {
-			gdstage.AddStep(newStartWatcherStage(dynamicKube, config, stopChan, log, broker)) // Register the watchers for different resources
-		}
-	} else {
-		for _, config := range configs {
-			gdstage.AddStep(newRegisterInformerStep(log, informer, config, broker)) // Register the informers for different resources
-		}
+	for _, config := range configs {
+		gdstage.AddStep(newRegisterInformerStep(log, informer, config, broker)) // Register the informers for different resources
 	}
 
 	// Local discovery
 	ldstage := LocalDiscoveryStage
 	configs = plConfigs[ldstage.Name]
-
-	if watchList {
-		for _, config := range configs {
-			ldstage.AddStep(newStartWatcherStage(dynamicKube, config, stopChan, log, broker)) // Register the watchers for different resources
-		}
-	} else {
-		for _, config := range configs {
-			ldstage.AddStep(newRegisterInformerStep(log, informer, config, broker)) // Register the informers for different resources
-		}
+	for _, config := range configs {
+		ldstage.AddStep(newRegisterInformerStep(log, informer, config, broker)) // Register the informers for different resources
 	}
+
+	// Start informers
+	strtInfmrs := StartInformersStage
+	strtInfmrs.AddStep(newStartInformersStep(stopChan, log, informer)) // Start the registered informers
 
 	// Create Pipeline
 	clusterPipeline := pipeline.New(Name, 1000)
-
-	// add stages to pipeline
 	clusterPipeline.AddStage(gdstage)
 	clusterPipeline.AddStage(ldstage)
-	if !watchList {
-		// Start informers
-		strtInfmrs := StartInformersStage
-		strtInfmrs.AddStep(newStartInformersStep(stopChan, log, informer)) // Start the registered informers
-		clusterPipeline.AddStage(strtInfmrs)
-	}
+	clusterPipeline.AddStage(strtInfmrs)
+
 	return clusterPipeline
 }
--- internal/pipeline/step.go
+++ internal/pipeline/step.go
@@ -1,20 +1,12 @@
 package pipeline
 
 import (
-	"context"
-	"sync"
-
 	broker "github.com/layer5io/meshkit/broker"
 	"github.com/layer5io/meshkit/logger"
 	internalconfig "github.com/layer5io/meshsync/internal/config"
 	"github.com/myntra/pipeline"
 
-	"github.com/layer5io/meshsync/pkg/model"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/watch"
-	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/dynamic/dynamicinformer"
 	"k8s.io/client-go/tools/cache"
 )
@@ -91,115 +83,5 @@ func (si *StartInformers) Exec(request *pipeline.Request) *pipeline.Result {
 // Cancel - step interface
 func (si *StartInformers) Cancel() error {
 	si.Status("cancel step")
-	return nil
-}
-
-type StartWatcher struct {
-	pipeline.StepContext
-	stopChan    chan struct{}
-	dynamicKube dynamic.Interface
-	log         logger.Handler
-	broker      broker.Handler
-	config      internalconfig.PipelineConfig
-}
-
-func newStartWatcherStage(dynamicKube dynamic.Interface, config internalconfig.PipelineConfig, stopChan chan struct{}, log logger.Handler, broker broker.Handler) *StartWatcher {
-	return &StartWatcher{
-		stopChan:    stopChan,
-		dynamicKube: dynamicKube,
-		config:      config,
-		log:         log,
-		broker:      broker,
-	}
-}
-
-func (w *StartWatcher) Exec(request *pipeline.Request) *pipeline.Result {
-	b := true
-	opts := metav1.ListOptions{
-		Watch:                true,
-		SendInitialEvents:    &b,
-		ResourceVersionMatch: metav1.ResourceVersionMatchNotOlderThan,
-	}
-	// attempts to begin watching the namespaces
-	// returns a `watch.Interface`, or an error
-
-	gvr, _ := schema.ParseResourceArg(w.config.Name)
-	watcher, err := w.dynamicKube.Resource(*gvr).Watch(context.TODO(), opts)
-	if err != nil {
-		return &pipeline.Result{
-			Error: err,
-			Data:  nil,
-		}
-	}
-
-	var wg sync.WaitGroup
-
-	// Launch the goroutine and pass the channel as an argument
-	wg.Add(1)
-	go w.backgroundWatchProcessor(watcher.ResultChan(), w.stopChan, &wg)
-	data := make(map[string]cache.Store)
-
-	return &pipeline.Result{
-		Error: nil,
-		Data:  data,
-	}
-}
-
-func (w *StartWatcher) backgroundWatchProcessor(result <-chan watch.Event, stopCh chan struct{}, wg *sync.WaitGroup) {
-	for {
-		select {
-		case <-stopCh:
-			return
-		default:
-			for event := range result {
-				obj := event.Object
-				switch event.Type {
-				// when an event is added...
-				case watch.Added:
-					err := w.publishItem(obj.(*unstructured.Unstructured), broker.Add, w.config)
-					if err != nil {
-						w.log.Error(err)
-					}
-					w.log.Info("Received ADD event for: ", obj.(*unstructured.Unstructured).GetName(), "/", obj.(*unstructured.Unstructured).GetNamespace(), " of kind: ", obj.(*unstructured.Unstructured).GroupVersionKind().Kind)
-				// when an event is modified...
-				case watch.Modified:
-					err := w.publishItem(obj.(*unstructured.Unstructured), broker.Update, w.config)
-					if err != nil {
-						w.log.Error(err)
-					}
-					w.log.Info("Received UPDATE event for: ", obj.(*unstructured.Unstructured).GetName(), "/", obj.(*unstructured.Unstructured).GetNamespace(), " of kind: ", obj.(*unstructured.Unstructured).GroupVersionKind().Kind)
-				// when an event is deleted...
-				case watch.Deleted:
-					var objCasted *unstructured.Unstructured
-					objCasted = obj.(*unstructured.Unstructured)
-					err := w.publishItem(objCasted, broker.Delete, w.config)
-					if err != nil {
-						w.log.Error(err)
-					}
-					w.log.Info("Received DELETE event for: ", obj.(*unstructured.Unstructured).GetName(), "/", obj.(*unstructured.Unstructured).GetNamespace(), " of kind: ", obj.(*unstructured.Unstructured).GroupVersionKind().Kind)
-				}
-			}
-		}
-		if len(w.stopChan) > 0 {
-			break
-		}
-	}
-	wg.Wait()
-}
-func (w *StartWatcher) publishItem(obj *unstructured.Unstructured, evtype broker.EventType, config internalconfig.PipelineConfig) error {
-	err := w.broker.Publish(config.PublishTo, &broker.Message{
-		ObjectType: broker.MeshSync,
-		EventType:  evtype,
-		Object:     model.ParseList(*obj),
-	})
-	if err != nil {
-		w.log.Error(ErrPublish(config.Name, err))
-		return err
-	}
-	return nil
-}
-
-func (w *StartWatcher) Cancel() error {
-	w.Status("cancel step")
 	return nil
 }
