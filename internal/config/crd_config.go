c82b97c89e8772f6378ac96a2d3fa27485c650d0
Daniel Kiptoon <kiptoonkipkurui@gmail.com>
2023-10-29 22:42:12 +0300

    refactor to allow configuration of resources
    Signed-off-by: Daniel Kiptoon <kiptoonkipkurui@gmail.com>

--- internal/config/crd_config.go
+++ internal/config/crd_config.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/layer5io/meshkit/utils"
 	mesherykube "github.com/layer5io/meshkit/utils/kubernetes"
+	"golang.org/x/exp/slices"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
@@ -46,7 +48,7 @@ func GetMeshsyncCRDConfigs() (*MeshsyncConfig, error) {
 	if !ok {
 		return nil, ErrInitConfig(errors.New("Unable to convert spec to map"))
 	}
-	configObj := specMap["config"]
+	configObj := specMap["watch-list"]
 	if configObj == nil {
 		return nil, ErrInitConfig(errors.New("Custom Resource does not have Meshsync Configs"))
 	}
@@ -55,10 +57,70 @@ func GetMeshsyncCRDConfigs() (*MeshsyncConfig, error) {
 		return nil, ErrInitConfig(err)
 	}
 	meshsyncConfig := MeshsyncConfig{}
-	err = utils.Unmarshal(string(configStr), &meshsyncConfig)
+	configMap := corev1.ConfigMap{}
+	err = utils.Unmarshal(string(configStr), &configMap)
 
 	if err != nil {
 		return nil, ErrInitConfig(err)
 	}
+
+	if _, ok := configMap.Data["blacklist"]; ok {
+		err = utils.Unmarshal(configMap.Data["blacklist"], &meshsyncConfig.BlackList)
+		if err != nil {
+			return nil, ErrInitConfig(err)
+		}
+	}
+
+	if _, ok := configMap.Data["whitelist"]; ok {
+		err = utils.Unmarshal(configMap.Data["whitelist"], &meshsyncConfig.WhiteList)
+		if err != nil {
+			return nil, ErrInitConfig(err)
+		}
+	}
+
+	// Handle global resources
+	globalPipelines := make(PipelineConfigs, 0)
+	for _, v := range Pipelines[GlobalResourceKey] {
+		if idx := slices.IndexFunc(meshsyncConfig.WhiteList, func(c ResourceConfig) bool { return c.Resource == v.Name }); idx != -1 {
+			config := meshsyncConfig.WhiteList[idx]
+			v.Events = config.Events
+			globalPipelines = append(globalPipelines, v)
+		}
+	}
+	if len(globalPipelines) > 0 {
+		meshsyncConfig.Pipelines = map[string]PipelineConfigs{}
+		meshsyncConfig.Pipelines[GlobalResourceKey] = globalPipelines
+	}
+
+	// Handle local resources
+	localPipelines := make(PipelineConfigs, 0)
+	for _, v := range Pipelines[LocalResourceKey] {
+		if idx := slices.IndexFunc(meshsyncConfig.WhiteList, func(c ResourceConfig) bool { return c.Resource == v.Name }); idx != -1 {
+			config := meshsyncConfig.WhiteList[idx]
+			v.Events = config.Events
+			localPipelines = append(localPipelines, v)
+		}
+	}
+
+	if len(localPipelines) > 0 {
+		meshsyncConfig.Pipelines = make(map[string]PipelineConfigs)
+		meshsyncConfig.Pipelines[LocalResourceKey] = localPipelines
+	}
+
+	// Handle listeners
+	listerners := make(ListenerConfigs, 0)
+	for _, v := range Listeners {
+		if idx := slices.IndexFunc(meshsyncConfig.WhiteList, func(c ResourceConfig) bool { return c.Resource == v.Name }); idx != -1 {
+			config := meshsyncConfig.WhiteList[idx]
+			v.Events = config.Events
+			listerners = append(listerners, v)
+		}
+	}
+
+	if len(listerners) > 0 {
+		meshsyncConfig.Listeners = make(map[string]ListenerConfig)
+		meshsyncConfig.Listeners = Listeners
+	}
+
 	return &meshsyncConfig, nil
 }
