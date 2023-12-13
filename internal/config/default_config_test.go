b7b50d7e3d87dafa03c6b65fc786a7d6be12f1c1
Daniel Kiptoon <kiptoonkipkurui@gmail.com>
2023-12-13 22:17:59 +0300

    add unit tests for getting resources from kubernetes Discovery
    Signed-off-by: Daniel Kiptoon <kiptoonkipkurui@gmail.com>

---
+++ internal/config/default_config_test.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"reflect"
+	"testing"
+
+	"golang.org/x/exp/slices"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	discoveryFake "k8s.io/client-go/discovery/fake"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+var (
+	client = fake.NewSimpleClientset()
+)
+
+func TestPopulateDefaultResources(t *testing.T) {
+
+	// Add fake resources
+	fakeResources := []*metav1.APIResourceList{
+		{
+			GroupVersion: "v1",
+			APIResources: []metav1.APIResource{
+				{Name: "pods", Namespaced: true, Kind: "Pod"},
+				{Name: "services", Namespaced: true, Kind: "Service"},
+				{Name: "namespaces", Namespaced: false, Kind: "Namespace"},
+			},
+		},
+	}
+
+	// mock the response from the discovery client
+	client.Discovery().(*discoveryFake.FakeDiscovery).Resources = fakeResources
+	discoveryClient := client.Discovery()
+
+	PopulateDefaultResources(discoveryClient)
+	if len(Pipelines[GlobalResourceKey]) != 1 {
+		t.Error("global resources not discovered, expected 1")
+	}
+
+	if len(Pipelines[LocalResourceKey]) != 2 {
+		t.Error("local resources not discovered, expected 1")
+	}
+
+	idx := slices.IndexFunc(Pipelines["global"], func(c PipelineConfig) bool { return c.Name == "namespaces.v1." })
+	namespaces := Pipelines["global"][idx]
+	if !reflect.DeepEqual(namespaces.Events, []string{"ADD", "UPDATE", "DELETE"}) {
+		t.Errorf("failure propagating required events expected [ADDED,DELETE,UPDATE] found %v", namespaces.Events)
+	}
+}
