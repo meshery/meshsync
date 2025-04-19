e4482beca736ef1b3af7e25066c21811dbbe1352
Nikita B <n2h9z4@gmail.com>
2025-04-19 18:09:12 +0200

    feature: 355: snapshot with local file: integration tests: add test case to receive from nats only specified resources
    Signed-off-by: Nikita B <n2h9z4@gmail.com>

--- integration-tests/default_cluster_test_cases_test.go
+++ integration-tests/default_cluster_test_cases_test.go
@@ -1,6 +1,8 @@
 package tests
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 	"time"
 
@@ -32,7 +34,7 @@ var defaultClusterTestCasesData []defaultClusterTestCaseStruct = []defaultCluste
 			resultData map[string]any,
 		) {
 			count := 0
-			resultData["count"] = 0
+			resultData["count"] = count
 			go func() {
 				for range out {
 					count++
@@ -47,6 +49,67 @@ var defaultClusterTestCasesData []defaultClusterTestCaseStruct = []defaultCluste
 				t.Logf("received %d messages from broker", count)
 				// TODO some more meaningful check
 				assert.True(t, count > 0, "must receive messages from queue")
+			}
+
+		},
+	},
+	{
+		name: "receive from nats only specified resources",
+		meshsyncCMDArgs: []string{
+			"--stopAfterSeconds",
+			"8",
+			"--outputResources",
+			"pod,replicaset",
+		},
+		natsMessageHandler: func(
+			t *testing.T,
+			out chan *broker.Message,
+			resultData map[string]any,
+		) {
+			resourcesCount := make(map[string]int)
+			resultData["count"] = resourcesCount
+			errCh := make(chan error)
+			go func(errCh0 chan<- error) {
+				for message := range out {
+					k8sResource, err := unmarshalObject(message.Object)
+					if err != nil {
+						errCh0 <- fmt.Errorf(
+							"error convering message.Object to model.KubernetesResource for %T",
+							message.Object,
+						)
+						return
+					}
+					resourcesCount[strings.ToLower(k8sResource.Kind)]++
+					resultData["count"] = resourcesCount
+				}
+			}(errCh)
+
+			err := <-errCh
+			if err != nil {
+				t.Fatal(err)
+			}
+		},
+		finalHandler: func(t *testing.T, resultData map[string]any) {
+			count, ok := resultData["count"].(map[string]int)
+			assert.True(t, ok, "must get count from result map")
+			if ok {
+				allowedKeys := map[string]bool{"pod": true, "replicaset": true}
+				otherKeys := make([]string, 0)
+				for k, v := range count {
+					t.Logf("received %d messages from Kind %s", v, k)
+					if !allowedKeys[k] {
+						otherKeys = append(
+							otherKeys,
+							fmt.Sprintf("[%s = %v]", k, v),
+						)
+					}
+				}
+				// TODO some more meaningful check
+				assert.True(t, count["pod"] > 0, "must receive kind pod messages from queue")
+				assert.True(t, count["replicaset"] > 0, "must receive kind replicaset messages from queue")
+				if len(otherKeys) > 0 {
+					t.Fatalf("received not allowed keys %s", strings.Join(otherKeys, ","))
+				}
 			}
 
 		},
