684d87283f22df7bf2cf7b4ebe4e37be4cc3a73c
Nikita B <n2h9z4@gmail.com>
2025-04-19 13:24:43 +0200

    featire: 355: snapshot with local file: add integration test case struct
    Signed-off-by: Nikita B <n2h9z4@gmail.com>

---
+++ integration-tests/default_cluster_integration_test.go
@@ -0,0 +1,109 @@
+package tests
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/layer5io/meshkit/broker"
+	"github.com/layer5io/meshkit/broker/nats"
+)
+
+var runIntegrationTest bool
+var meshsyncBinaryPath string
+var testMeshsyncTopic = "meshery.meshsync.core"
+var testMeshsyncNatsURL = "localhost:4222"
+
+func init() {
+	runIntegrationTest = os.Getenv("RUN_INTEGRATION_TESTS") == "true"
+	meshsyncBinaryPath = os.Getenv("MESHSYNC_BINARY_PATH")
+}
+
+/**
+ * this test requires k8s cluster (with installed CRDs: meshsync) and nats streaming;
+ * --
+ * use docker compose to start nats;
+ * ---
+ * this test runs all test cases on the same k8s cluster, but with different input params for meshsync;
+ * if you need a specific cluster setup you (probably) need to write a separate test,
+ * or fit in the current cluster set up without failing existing tests;
+ * --
+ * test flow of every test case is as follow:
+ * - subscribe to nats (each test case has a separate queue group, so it receives every message);
+ * - run meshsync binary;
+ * - receive messages from nats and perform assertions;
+ */
+func TestWithNatsDefaultK8SClusterIntegration(t *testing.T) {
+	if !runIntegrationTest {
+		t.Skip("skipping integration test")
+	}
+
+	br, err := nats.New(nats.Options{
+		URLS:           []string{testMeshsyncNatsURL},
+		ConnectionName: "meshsync",
+		Username:       "",
+		Password:       "",
+		ReconnectWait:  2 * time.Second,
+		MaxReconnect:   60,
+	})
+	if err != nil {
+		t.Fatal("error connecting to nats", err)
+	}
+
+	for i, tc := range defaultClusterTestCasesData {
+		t.Run(tc.name, func(t *testing.T) {
+			out := make(chan *broker.Message)
+			// Step 1: subscribe to the queue
+			err = br.SubscribeWithChannel(
+				testMeshsyncTopic,
+				fmt.Sprintf("default-cluster-queue-group-%d", i),
+				out,
+			)
+			if err != nil {
+				t.Fatalf("error subscribing to topic: %v", err)
+			}
+
+			// Step 2: process messages
+			resultData := make(map[string]any, 1)
+			go tc.natsMessageHandler(t, out, resultData)
+
+			os.Setenv("BROKER_URL", testMeshsyncNatsURL)
+
+			// Step 3: run the meshsync command
+			cmd := exec.Command(meshsyncBinaryPath, tc.meshsyncCMDArgs...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmd.Stdin = os.Stdin
+			errCh := make(chan error)
+			runMeshsyncCMD := func(cmd0 *exec.Cmd, errCh0 chan<- error) {
+				errCh0 <- cmd0.Run()
+			}
+			go runMeshsyncCMD(cmd, errCh)
+
+			timeout := time.Duration(time.Hour * 24)
+			if tc.waitMeshsyncTimeout > 0 {
+				timeout = tc.waitMeshsyncTimeout
+			}
+
+			select {
+			case err := <-errCh:
+				if err != nil {
+					t.Fatalf("error running binary: %v", err)
+				}
+			case <-time.After(timeout):
+				if err := cmd.Process.Signal(syscall.SIGTERM); err != nil {
+					t.Fatalf("error terminating meshsync command: %v", err)
+				}
+				t.Logf("processing after timeout %d", timeout)
+			}
+
+			// Step 4: do final assertion, if any
+			tc.finalHandler(t, resultData)
+
+			t.Logf("done %s", tc.name)
+		})
+	}
+}
---
+++ integration-tests/default_cluster_test_cases_test.go
@@ -0,0 +1,54 @@
+package tests
+
+import (
+	"testing"
+	"time"
+
+	"github.com/layer5io/meshkit/broker"
+	"github.com/stretchr/testify/assert"
+)
+
+type defaultClusterTestCaseStruct struct {
+	name                string
+	meshsyncCMDArgs     []string      // args to pass to meshsync binary
+	waitMeshsyncTimeout time.Duration // if <= 0: waits till meshsync ends execution, otherwise moves  further after specified duration
+	// the reason for resultData map is that natsMessageHandler is processing chan indefinitely
+	// and there is no graceful exit from function;
+	natsMessageHandler func(
+		t *testing.T,
+		out chan *broker.Message,
+		resultData map[string]any,
+	)
+	finalHandler func(t *testing.T, resultData map[string]any)
+}
+
+var defaultClusterTestCasesData []defaultClusterTestCaseStruct = []defaultClusterTestCaseStruct{
+	{
+		name:            "number of messages received from nats is greater than zero",
+		meshsyncCMDArgs: []string{"--stopAfterSeconds", "8"},
+		natsMessageHandler: func(
+			t *testing.T,
+			out chan *broker.Message,
+			resultData map[string]any,
+		) {
+			count := 0
+			resultData["count"] = 0
+			go func() {
+				for range out {
+					count++
+					resultData["count"] = count
+				}
+			}()
+		},
+		finalHandler: func(t *testing.T, resultData map[string]any) {
+			count, ok := resultData["count"].(int)
+			assert.True(t, ok, "must get count from result map")
+			if ok {
+				t.Logf("received %d messages from broker", count)
+				// TODO some more meaningful check
+				assert.True(t, count > 0, "must receive messages from queue")
+			}
+
+		},
+	},
+}
